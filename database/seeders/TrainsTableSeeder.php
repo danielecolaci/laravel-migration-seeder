d44e9a5820173bd759952a6d334aac696c6f4838
danielecolaci <danielecolaci@outlook.com>
2024-05-13 15:37:36 +0200

    Set Faker to seeder

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Database\Seeders;
+use Faker\Generator as Faker;
 use App\Models\Train;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -11,9 +12,24 @@ class TrainsTableSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        $trains = config('db.trains');
+        for ($i=0; $i < 10; $i++) { 
+            $train = new Train();
+            $train->company = $faker->randomElement(['Italo', 'Trenitalia', 'Trenord', 'Frecciargento']);
+            $train->departure_station = $faker->city();
+            $train->arrival_station = $faker->city();
+            $train->departure_time = $faker->time('H:i:s');
+            $train->arrival_time = $faker->time('H:i:s');
+            $train->train_code = $faker->bothify('??##');
+            $train->num_carriages = $faker->numberBetween(1, 20);
+            $train->on_time = $faker->boolean();
+            $train->cancelled = $faker->boolean();
+            $train->save(); 
+        }
+
+
+        /* $trains = config('db.trains');
 
         foreach($trains as $train) {
             $newTrain = new Train();
@@ -27,7 +43,7 @@ class TrainsTableSeeder extends Seeder
             $newTrain->on_time = $train['on_time'];
             $newTrain->cancelled = $train['cancelled'];
             $newTrain->save(); 
-        }
+        } */
     }
     
 }
